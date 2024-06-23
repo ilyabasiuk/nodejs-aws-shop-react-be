3534bf698725be52923a1e1c3f1ac103dab87757
Ilya Basiuk <ibasiuk2@slb.com>
2024-06-23 23:35:53 +0200

    Task 4.3.3 implementation for createProduct

--- product-service/lambda/createProduct.ts
+++ product-service/lambda/createProduct.ts
@@ -1,3 +1,5 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyHandler,
@@ -6,9 +8,66 @@ import {
 
 import { v4 } from "uuid";
 
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+  console.log("eventBody", event.body);
+
+  const { title, description, price, count } = JSON.parse(event.body || "{}");
+  if (!title || !description || !price || !count) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: "Product data is required" }),
+    };
+  }
+
+  try {
+    // create product in dynamodb
+    const id = v4();
+    const command = new PutCommand({
+      TableName: process.env.PRODUCT_TABLE_NAME,
+      Item: {
+        id,
+        title,
+        description,
+        price,
+      },
+    });
+
+    const response = await docClient.send(command);
+    console.log(response);
+    // create stock in dynamodb
+    const commandStock = new PutCommand({
+      TableName: process.env.STOCK_TABLE_NAME,
+      Item: {
+        product_id: id,
+        count,
+      },
+    });
+    const responseStock = await docClient.send(commandStock);
+    console.log(responseStock);
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: "Product not created" }),
+    };
+  }
+
   return {
     statusCode: 200,
     headers: {
