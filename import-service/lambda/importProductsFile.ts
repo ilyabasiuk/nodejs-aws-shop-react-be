e9b022b1888eec068d072b9180e93bb7c9ac3108
Ilya Basiuk <ibasiuk2@slb.com>
2024-06-28 17:33:45 +0200

    task 5.1 - import service init

---
+++ import-service/lambda/importProductsFile.ts
@@ -0,0 +1,20 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+} from "aws-lambda";
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  console.log("Request:", event);
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({ message: "Hello from importProductsFile" }),
+  };
+};
