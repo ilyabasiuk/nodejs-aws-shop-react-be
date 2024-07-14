fb9cd872295a349fc77154111f31c4a43126553b
Ilya Basiuk <ibasiuk2@slb.com>
2024-07-14 23:57:11 +0200

    task-7.1: crea basicAthorizer lambda

---
+++ authorization-service/lib/authorization-service-stack.ts
@@ -0,0 +1,31 @@
+import * as dotenv from "dotenv";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+dotenv.config();
+
+export class AuthorizationServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const login = "ilyabasiuk";
+    const password = process.env[login];
+
+    const basicAuthorizer = new Function(this, "BasicAuthorizer", {
+      runtime: Runtime.NODEJS_16_X,
+      handler: "basicAuthorizer.handler",
+      code: Code.fromAsset("lambda"),
+      environment: {
+        USERNAME: login,
+        PASSWORD: password!,
+      },
+    });
+
+    new cdk.CfnOutput(this, "basicAuthorizer", {
+      value: basicAuthorizer.functionArn,
+      exportName: "BasicAuthorizerArn",
+    });
+  }
+}
